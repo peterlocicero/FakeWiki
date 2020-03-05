0849e406a25df97f5e6a430861beace309d2c579
peterlocicero <peterlociceroschool@gmail.com>
2020-03-05 00:44:33 -0500

    updates/fixes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 const htmlFolder = 'pages';
 const fs= require('fs');
-
+var fileArray= [];
 
 //index page
 app.get('/', function(req, res) {
@@ -14,32 +14,48 @@ app.get('/', function(req, res) {
 
 fs.readdir(htmlFolder, (err,files) =>{
   files.forEach(file => {
-
-    fullFile = path.join(__dirname, htmlFolder, file);
-    console.log(file);
-
-    app.get(('/'+file),function(req, res) {
-      console.log(`requested ${file} page`);
-     res.sendFile(fullFile);
-      });
-    });
+    fileArray.push(`/pages/${file}`)
+   });
   });
+//
+// fs.readdir(htmlFolder, (err,files) =>{
+//   files.forEach(file => {
+
+//     fullFile = path.join(__dirname, htmlFolder, file);
+//     console.log(file);
+
+//     app.get(('/'+file),function(req, res) {
+//       console.log(`requested ${file} page`);
+//      res.sendFile(fullFile);
+//       });
+//     });
+//   });
+
 
 
 //has to go at the end
 app.get('*', function(req, res) {
-	console.log('requested illegal page');
-    res.send('dont work');
+  if (fileArray.includes(`/pages${req.originalUrl}`)){
+     res.sendFile(`/pages${req.originalUrl}`, {root : __dirname});
+   }
+   else {
+    console.log(`/pages${req.originalUrl}`);
+      res.send('dont work');
+   }
+  });
+
+app.use(express.json())
+
+app.post('/login', function(req,res){
+  var title=req.body.title;
+  var content=req.body.content;
+  console.log(req.body.title);
+  fs.appendFile(`pages/${title}.html`, content, function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  }); 
+  
 });
-
-// app.use(express.json())
-
-// app.post('/login', function(req,res){
-//   var user=req.body.user;
-//   var password=req.body.password;
-//   console.log("User name = "+user+", password is "+password);
-//   res.send(JSON.stringify("got username and password"));
-// });
 
 
 app.listen(8080);
